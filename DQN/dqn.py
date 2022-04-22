fix state normalization bug

DQN/dqn.py
@@ -1,3 +1,4 @@
+import os
 import math
 import numpy as np
 import random
@@ -9,16 +10,40 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.autograd as autograd 
-import torch.nn.functional as F
 
 import gym
-from collections import deque
+import collections
 
+class Utility():
+	def __init__(self, args):
+		self.stack_frame = args.stack_frame
+		self.device = args.device
+
+	def _transform_frame(self, frame):
+		mini_frame = cv2.cvtColor(cv2.resize(frame, (84,84)), 
+			cv2.COLOR_BGR2GRAY)
+		return mini_frame
+
+	def _update_state(self, frame, state):
+		# 84 * 84
+		mini_frame = self._transform_frame(frame)
+		mini_frame_tensor = torch.tensor(
+			mini_frame, dtype=torch.uint8).to(self.device)
+
+		if state.numel() == 0:
+			# 1 * 1 * 84 * 84
+			state = mini_frame_tensor.unsqueeze(0).unsqueeze(0)
+		elif len(state[0]) < self.stack_frame:
+			state = torch.cat((state.squeeze(0), mini_frame_tensor.unsqueeze(0))).unsqueeze(0)
+		else:
+			state = torch.cat(
+				(state.squeeze(0)[1:,:,:], mini_frame_tensor.unsqueeze(0))).unsqueeze(0)
+
+		return state
 
 class ExpReplay():
 	def __init__(self, capacity):
-		super(ExpReplay, self).__init__()
-		self.memory = deque(maxlen=capacity)
+		self.memory = collections.deque(maxlen=capacity)
 
 	def push(self, state, action, reward, next_state, done):
 		self.memory.append((state, action, reward, next_state, done))
@@ -32,12 +57,12 @@ class ExpReplay():
 		return len(self.memory)
 
 class DQNAgent(nn.Module):
-	def __init__(self, action_size):
+	def __init__(self, action_size, stack_frame):
 		super(DQNAgent, self).__init__()
 		self.action_size = action_size
 
 		self.layers = nn.Sequential(
-			nn.Conv2d(4, 32, 8, stride=4),
+			nn.Conv2d(stack_frame, 32, 8, stride=4),
 			nn.ReLU(),
 			nn.Conv2d(32, 64, 4, stride=2),
 			nn.ReLU(),
@@ -61,13 +86,13 @@ class DQNAgent(nn.Module):
 		return action
 
 class TrainAgent():
-	def __init__(self, env, agent, target_agent, exp, device, args):
-		self.env = env
-		self.agent = agent
-		self.target_agent = target_agent
-		self.exp = exp
-		self.device = device
-		# Train Arguments		
+	def __init__(self, args):
+		self.device = args.device
+		self.env = gym.make(args.env_name)
+		self.agent = DQNAgent(self.env.action_space.n, args.stack_frame).to(self.device)
+		self.target_agent = DQNAgent(self.env.action_space.n, args.stack_frame).to(self.device)
+		self.exp = ExpReplay(args.exp_capacity)
+		# Training Parameters
 		self.episode_training = args.episode_training
 		self.gamma = 0.99
 		self.batch_size = 32
@@ -75,79 +100,124 @@ class TrainAgent():
 		self.epsilon = 1
 		self.epsilon_start = args.epsilon_start
 		self.epsilon_end = args.epsilon_end
+		self.epsilon_min = 0.1
 		self.update_agent = args.update_agent
 		self.update_target_agent = args.update_target_agent
-		self.learn_start = args.learn_start
+		# Loss & Optimize
 		self.optimizer = optim.RMSprop(
-			self.agent.parameters(), lr=0.00025, alpha=0.95, eps=0.01, momentum=0.95)
+			self.agent.parameters(), lr=0.00025, alpha=0.95, momentum=0.95, eps=1e-2)
 		self.loss = nn.HuberLoss()
 		# Record Steps
 		self.steps = 0
+		# Utility Functions
+		self.utility = Utility(args)
+		# Path to save model
+		self.model_path = 'trained_model/' + args.pretrained_agent
+		# Prepare LogFile
+		self._log('eposide,reward,step\n', 'train_eposide_reward.txt', False)
+
+	def _log(self, info, filename='train_eposide_reward.txt', is_append=True):
+		if is_append:
+			with open('Log/' + filename, 'a') as logf:
+				logf.write(info)
+		else:
+			with open('Log/' + filename, 'w') as logf:
+				logf.write(info)
 
 	def _epsilon_decay(self):
-		self.epsilon = 1 - self.steps / self.epsilon_end
-		self.epsilon = max(self.epsilon, 0.1)
+		self.epsilon = 1 - (self.steps / self.epsilon_end) * 0.9
+		self.epsilon = max(self.epsilon, self.epsilon_min)
 		
-	def _transform_frame(self, frame):
-		mini_frame = cv2.cvtColor(cv2.resize(frame, (84, 84)), cv2.COLOR_BGR2GRAY)
-		return mini_frame
-
-	def _update_state(self, frame, state):
-		mini_frame = self._transform_frame(frame)
-		mini_frame_tensor = torch.tensor(mini_frame, dtype=torch.uint8).unsqueeze(0).to(self.device)
-
-		if len(state.squeeze(0)) == 0:
-			state = mini_frame_tensor.unsqueeze(0)
-		elif len(state.squeeze(0)) < self.stack_frame:
-			state = torch.cat((state.squeeze(0), mini_frame_tensor)).unsqueeze(0)
-		else:
-			state = torch.cat((state.squeeze(0)[1:,:,:], mini_frame_tensor)).unsqueeze(0)
-
-		return state
-
 	def train(self):
 		for episode_i in range(self.episode_training):
-			obs, done, state, episode_reward = self.env.reset(), False, torch.tensor([]), 0
+			obs, done, state, episode_reward = self.env.reset(), False, torch.tensor([]).to(self.device), 0
 
 			while not done:
-				state = self._update_state(obs, state).to(self.device)
+				state = self.utility._update_state(obs, state)
+
+				# if frames is not enough to make decision
 				if len(state[0]) < self.stack_frame:
 					obs, reward, done, _ = self.env.step(0)
 				else:
-					action = self.agent.act(state.float(), self.epsilon)
+					# interaction with environment. state normalization is very important
+					action = self.agent.act(state / 255.0, self.epsilon)
 					obs, reward, done, _ = self.env.step(action)
-					self.exp.push(state, action, reward, self._update_state(obs, state), done)
+
+					# store experience
+					self.exp.push(state, action, reward, self.utility._update_state(obs, state), done)
 					self.steps += 1
 					episode_reward += reward
 
-					if self.steps % self.update_agent == 0:
-						if len(self.exp) >= self.learn_start:
-							self._train_one_epoch()
-
+					# epsilon decay
 					if self.steps >= self.epsilon_start:
 						self._epsilon_decay()
 
+					# learn from experience
+					if self.steps % self.update_agent == 0:
+						# don't learn before epsilon decay
+						if len(self.exp) >= self.epsilon_start:
+							self._train_one_epoch()
+
+					# store target agent & save target agent weight
 					if self.steps % self.update_target_agent == 0:
 						self.target_agent.load_state_dict(self.agent.state_dict())
 						if self.steps % (self.update_target_agent * 10) == 0:
-							torch.save(self.target_agent, 'target_agent.pth')
+							torch.save(self.target_agent.state_dict(), self.model_path)
 
-			with open('eposide_log.txt', 'a') as f:
-				f.write(f'{episode_i},{episode_reward},{self.steps}')
-				f.write('\n')
+			self._log(f'{episode_i},{episode_reward},{self.steps}\n')
 
 	def _train_one_epoch(self):
-		# state, next_state is tensor, action, reward is scalar
+		# state, next_state is tensor, action, reward is scalar tuple
 		states, actions, rewards, next_states, dones = self.exp.sample(self.batch_size)
+		# state normalization is very important for proper gradients
 		states = torch.cat(states).float() / 255.0
 		next_states = torch.cat(next_states).float() / 255.0
+		# calculate target_q = reward + discount * q_star
 		target_q = (
 			torch.tensor(rewards, dtype=torch.float32).to(self.device) + 
 			self.gamma * self.target_agent(next_states).max(1)[0] * 
 			(1 - torch.tensor(dones, dtype=torch.float32)).to(self.device)
 			)
-		target_q = target_q.detach().to(self.device)
+		target_q = target_q.detach()
+		# calculate q(s,a): select the action value from agent on states
 		q = self.agent(states).gather(1, torch.tensor(actions).unsqueeze(1).to(self.device)).squeeze()
 		self.optimizer.zero_grad()
 		self.loss(q, target_q).backward()
 		self.optimizer.step()
+		# temp monitor
+		if self.steps % 20000 == 0:
+			print(q)
+			print('*'*30)
+			print(target_q)
+
+class EvaluateAgent():
+	"""docstring for EvaluateAgent
+	Evaluate the pretrained agent performance
+	"""
+	def __init__(self, args):
+		self.device = args.device
+		self.path = 'trained_model/' + args.pretrained_agent
+		self.env = gym.make(args.env_name, render_mode='human')
+		self.agent = DQNAgent(self.env.action_space.n, args.stack_frame).to(self.device)
+		self.utility = Utility(args)
+		self.stack_frame = args.stack_frame
+		
+	def evaluate(self):
+		self.agent.load_state_dict(torch.load(self.path, map_location=self.device))
+
+		# begin evaluation
+		obs, done, state, episode_reward = self.env.reset(), False, torch.tensor([]), 0
+		while not done:
+			state = self.utility._update_state(obs, state)
+			if len(state[0]) < self.stack_frame:
+				obs, reward, done, _ = self.env.step(0)
+			else:
+				# state narmalization is very import
+				action_value = self.agent(state.float() / 255.0).squeeze(0).tolist()
+				info = ''
+				for action, value in enumerate(action_value):
+					info += 'Button ' + str(action) + ':' + str(round(value,2)) + ', ' 
+				print(info)
+				action = self.agent.act(state.float() / 255.0, 0)
+				obs, reward, done, _ = self.env.step(action)
+

main.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import torch
 import gym
@@ -8,25 +9,34 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def parse():
     parser = argparse.ArgumentParser(description='DQN Arguments')
+
+    parser.add_argument('--train', default=1, type=int) # 1:train, 0:evaluate
     parser.add_argument('--env_name', default='ALE/Pong-v5', help='environment name')
     parser.add_argument('--exp_capacity', default=200000, type=int)
-    parser.add_argument('--episode_training', default=1000000, type=int)
+    parser.add_argument('--device', default=device)
+    ## epsilon decay parameter
+    parser.add_argument('--episode_training', default=100000, type=int)
     parser.add_argument('--epsilon_start', default=50000, type=int)
     parser.add_argument('--epsilon_end', default=1000000, type=int)
+    ## stack frames
     parser.add_argument('--stack_frame', default=4, type=int)
+    ## update frequency for agent & target agent
     parser.add_argument('--update_target_agent', default=10000, type=int)
-    parser.add_argument('--update_agent', default=1, type=int)
-    parser.add_argument('--learn_start', default=50000, type=int)
+    parser.add_argument('--update_agent', default=4, type=int)
+    ## pretained agent to be loaded to evaluate
+    parser.add_argument('--pretrained_agent', default='target_agent_weight.pth')
+
     args = parser.parse_args()
     return args
 
 def run(args):
-	env = gym.make(args.env_name)
-	exp = dqn.ExpReplay(capacity=args.exp_capacity)
-	agent = dqn.DQNAgent(env.action_space.n).to(device)
-	target_agent = dqn.DQNAgent(env.action_space.n).to(device)
-	train_agent = dqn.TrainAgent(env, agent, target_agent, exp, device, args)
-	train_agent.train()
+    if args.train == 1:
+        train_agent = dqn.TrainAgent(args)
+        train_agent.train()
+    else:
+        evaluate_agent = dqn.EvaluateAgent(args)
+        evaluate_agent.evaluate()
+
 
 def main():
     args = parse()